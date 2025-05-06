f4d7e41b5569ca2816cd9e27b3b56d001e5a1f30
AndreyLucasPail <andreypail@hotmail.com>
2025-05-06 14:51:11 -0300

    update

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:alarm_clock_app/ui/utils/customcolors.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: CustomColors.lavender);
+  }
+}
---
+++ lib/ui/utils/customcolors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color rotPurple = Color(0xFF432344);
+  static const Color redOrange = Color(0xFFFF2525);
+  static const Color lavender = Color(0xFFE7EEFB);
+  static const Color supernova = Color(0xFFFFC03D);
+}
