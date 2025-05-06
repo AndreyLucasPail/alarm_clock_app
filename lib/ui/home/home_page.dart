020e16829aae4f9de07cad40409c49e958a348bd
AndreyLucasPail <andreypail@hotmail.com>
2025-05-06 15:22:22 -0300

    update

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -11,6 +11,74 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: CustomColors.lavender);
+    return Scaffold(
+      backgroundColor: CustomColors.white,
+      appBar: appBar(),
+      body: body(),
+      floatingActionButton: floatingButton(),
+    );
+  }
+
+  Widget body() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: [alarmRow()]),
+      ),
+    );
+  }
+
+  PreferredSizeWidget appBar() {
+    return AppBar(
+      backgroundColor: CustomColors.white,
+      title: Align(
+        alignment: Alignment.centerRight,
+        child: Text(
+          "Alarm",
+          style: TextStyle(
+            color: CustomColors.black,
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget floatingButton() {
+    return SizedBox(
+      height: 80,
+      width: 80,
+      child: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: CustomColors.redOrange,
+        shape: CircleBorder(),
+        child: Icon(Icons.add, color: CustomColors.white, size: 40),
+      ),
+    );
+  }
+
+  Widget alarmRow() {
+    return Row(
+      children: [
+        alarmCard(),
+        Transform.rotate(
+          angle: -3.12 / 2,
+          child: Switch(value: false, onChanged: (value) {}),
+        ),
+      ],
+    );
+  }
+
+  Widget alarmCard() {
+    return Container(
+      height: 260,
+      width: 250,
+      decoration: BoxDecoration(
+        color: CustomColors.supernova,
+        borderRadius: BorderRadius.circular(40.0),
+      ),
+    );
   }
 }
